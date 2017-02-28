88e2457f3819f6a79077a80b738505e043c8fb79
Milana <milana.stojadinov@gmail.com>
2017-02-28 22:55:47 +0100

    Improve? html chart spec

--- spec/haskii/html_chart_spec.rb
+++ spec/haskii/html_chart_spec.rb
@@ -2,15 +2,32 @@ require "spec_helper"
 
 describe Haskii::HtmlChart do
 
-  describe "#table" do
-    let(:matrix) { [[" ", " ", "*"],
-                    [" ", " ", "*"],
-                    [" ", "*", "*"],
-                    ["*", "*", "*"]] }
+  let(:matrix) { [[" ", " ", "*"],
+                  [" ", " ", "*"],
+                  [" ", "*", "*"],
+                  ["*", "*", "*"]] }
 
     let(:html_string) { "<table><tr><td> </td><td> </td><td>*</td></tr><tr><td> </td><td> </td><td>*</td></tr><tr><td> </td><td>*</td><td>*</td></tr><tr><td>*</td><td>*</td><td>*</td></tr></table>" }
+
+  describe "#create" do
+    before do
+      @html_double = double(File.new("chart.html", "w+"))
+      allow(File).to receive(:new).with("chart.html", "w+").and_return(@html_double)
+    end
+
+    it "tries to create html file" do
+      expect(File).to receive(:new).with("chart.html", "w+")
+      expect(@html_double).to receive(:puts).with(html_string)
+      expect(@html_double).to receive(:close)
+
+      described_class.new(matrix).create
+    end
+  end
+
+  describe "#table" do
     it "returns appropriate html table" do
       expect(described_class.new(matrix).table).to eql(html_string)
     end
   end
+
 end
