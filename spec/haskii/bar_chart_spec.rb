11ba4ee81a7f0336f46bdc47d7c0ce0c930077ef
Milana <milana.stojadinov@gmail.com>
2017-03-13 00:36:13 +0100

    Raise NotImplementedError for abstract class

--- spec/haskii/bar_chart_spec.rb
+++ spec/haskii/bar_chart_spec.rb
@@ -1,5 +1,21 @@
 require "spec_helper"
 
 describe Haskii::BarChart do
+  let(:numbers) { [1,2,3] }
+  let(:emoji) { "*" }
 
+  describe "#render" do
+    before do
+      @emoji_double = [ " ", " ", "*",
+                        " ", "*", "*",
+                        "*", "*", "*" ]
+      @matrix_double = double(Haskii::MimiMatrix.new(numbers,emoji))
+      allow(Haskii::MimiMatrix).to receive(:new).with(numbers,emoji).and_return(@matrix_double)
+      allow(@matrix_double).to receive(:create).and_return(@emoji_double)
+    end
+
+    it "raises a NotImplementedError" do
+      expect { described_class.new(numbers,emoji).render }.to raise_error(NotImplementedError)
+    end
+  end
 end
